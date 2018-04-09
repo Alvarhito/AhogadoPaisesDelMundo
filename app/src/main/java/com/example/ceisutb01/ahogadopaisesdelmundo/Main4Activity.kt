169872ad9090773af3f5ae9d819e299115e6d950
ceisutb01 <aovargas05@gmail.com>
2018-04-09 16:27:08 -0500

    actualizacion 15

--- app/src/main/java/com/example/ceisutb01/ahogadopaisesdelmundo/Main4Activity.kt
+++ app/src/main/java/com/example/ceisutb01/ahogadopaisesdelmundo/Main4Activity.kt
@@ -4,20 +4,37 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import com.google.firebase.iid.FirebaseInstanceId
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_main4.*
+
 
 class Main4Activity : AppCompatActivity() {
+    var ref = FirebaseDatabase.getInstance().reference
+    var mensajeRef = ref.child("Servidor")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main4)
+        //modifica("Funciona")
+
     }
+    /*
     fun onTokenRefresh() {
         val refreshedToken = FirebaseInstanceId.getInstance().token
         val prueba= Prueba as Button
         prueba.text="Se actualizo!!"
 
+    }*/
+
+    fun modifica(mensaje: String){
+        mensajeRef.setValue(mensaje)
     }
 
     override fun onResume() {
@@ -30,5 +47,20 @@ class Main4Activity : AppCompatActivity() {
                 or View.SYSTEM_UI_FLAG_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
         decorView.systemUiVisibility = uiOptions
+
+
+        mensajeRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.getValue(String::class.java)
+                val prueba= prueba as TextView
+                prueba.text=value
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                val prueba= prueba as TextView
+                prueba.text="Error"
+            }
+        })
     }
+
 }
