122fa69cc8b081d106984b655c014fd1358b2487
Alvarhito <aovargas05@gmail.com>
2018-04-10 01:25:57 -0500

    Actualizacion 16

--- app/src/main/java/com/example/ceisutb01/ahogadopaisesdelmundo/Main4Activity.kt
+++ app/src/main/java/com/example/ceisutb01/ahogadopaisesdelmundo/Main4Activity.kt
@@ -13,22 +13,30 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_main4.*
+import java.util.*
 
 
 object Constants {
     @JvmStatic
-    val FIREBASE_ITEM: String = "hola"
+    val FIREBASE_ITEM: String = "Servicio"
 }
 class Main4Activity : AppCompatActivity() {
     var ref = FirebaseDatabase.getInstance().reference
     val newqst = ref.child(Constants.FIREBASE_ITEM).push()
-    val aux=newqst.setValue("jhjh")
-    var mensajeRef = ref.child("Servidor")
+    var aaa=newqst
+
+    var usuario=ale()
+    //val aux0=newqst.child(usuario) .setValue(newqst.key)
+    val aux=newqst.child("player1") .setValue("Primer Jugador")
+    val aux1=newqst.child("player2").setValue("Segundo Jugador")
+
+    var mensajeRef = ref.child(usuario)
+    var auu=mensajeRef.setValue(newqst.key)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main4)
-        //modifica("Funciona")
 
     }
 
@@ -44,10 +52,24 @@ class Main4Activity : AppCompatActivity() {
     fun modifica(mensaje: String){
         mensajeRef.setValue(mensaje)
     }
+    fun ale() :String{
+        val random = Random()
+        var num=""
+        for (i in 0..3){
+            num += (random.nextInt(9 - 0)).toString()
+        }
+        return  num
+    }
 
+
+    override fun onDestroy() {
+        //var hola0=newqst.child(usuario).removeValue()
+        var hola=newqst.child("player1").removeValue()
+        var hola1=newqst.child("player2").removeValue()
+        super.onDestroy()
+    }
     override fun onResume() {
         super.onResume()
-
         val decorView = window.decorView
         val uiOptions = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -55,6 +77,7 @@ class Main4Activity : AppCompatActivity() {
                 or View.SYSTEM_UI_FLAG_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
         decorView.systemUiVisibility = uiOptions
+
 
 
         mensajeRef.addValueEventListener(object : ValueEventListener {
